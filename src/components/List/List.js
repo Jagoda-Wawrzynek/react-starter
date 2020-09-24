cd763a672982d87111c110b017c7c4b95a876775
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-09-24 21:10:18 +0200

    Delete some consol.log

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -14,7 +14,6 @@ class List extends React.Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image}/>
